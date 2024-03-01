14a14d018c859aaf1b07cbce42a4e0d12acdbdd1
Tomasz Mach <m4chtomasz@gmail.com>
2024-03-01 14:43:18 +0100

    first javascript

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log('CIAO JAVASCRIPT')
+
+// esercizio 1
+let number = 100 //number
+console.log(number)
+
+// esrcizio 2
+const PI = 3.14 //number
+console.log(PI)
+
+// esercizio 2/B
+// const PI = 3.1416 
+
+// Le variabili dichiarate con const non possono essere riassegnate.
+
+// esercizio 2/C
+let radius = 8 //number
+circle = (radius * 2 * PI)
+console.log(circle) 
+
+// esercizio 3
+let name = 'Carlo' //stringa
+name = 'Marco' //stringa
+console.log(name)
+
+
+
+
+
+
+
